469d853bbf6c69f42443c6cf6b7ecb931218e2ba
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-04-15 14:15:16 +0300

    done

--- src/__tests__/ArticleList.test.js
+++ src/__tests__/ArticleList.test.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
-import ArticleList from "../components/Article";
+import ArticleList from "../components/ArticleList";
 
 const posts = [
   {
